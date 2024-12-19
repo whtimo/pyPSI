2d1cb848240fc123493048f902acf0f195fb844d
Timo Balz <balz@whu.edu.cn>
2024-12-19 11:17:22 +0800

    Prepare processing

--- ps_parameter_estimation_periodogram.py
+++ ps_parameter_estimation_periodogram.py
@@ -382,8 +382,8 @@ date_columns = df_ps.columns[3:]
 # Convert the date strings to datetime objects and store in a list
 dates = [datetime.strptime(date, '%Y-%m-%d') for date in date_columns]
 
-ref_point = 0
-#ref_point = find_matching_point_index('/home/timo/Data/LasVegasDesc/ref_point.txt', '/home/timo/Data/LasVegasDesc/ps.csv', '/home/timo/Data/LasVegasDesc/ps_phases.csv')
+#ref_point = 0
+ref_point = find_matching_point_index('/home/timo/Data/LasVegasDesc/ref_point.txt', '/home/timo/Data/LasVegasDesc/ps.csv', '/home/timo/Data/LasVegasDesc/ps_phases.csv')
 #print("Reading the network") # Adding some comments because it is a long process
 #ps_network = PSNetwork(dates, "/path/to/xml/files")
 ps_info = PSInfo(dates, "/home/timo/Data/LasVegasDesc/topo",  "/home/timo/Data/LasVegasDesc/ps_phases.csv")

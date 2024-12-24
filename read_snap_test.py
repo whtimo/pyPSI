4cdfead12ed0ec340d1ddd704b1cea20bcf32152
Timo Balz <balz@whu.edu.cn>
2024-12-24 12:46:32 +0800

    Get a snappy configuration running

--- read_snap_test.py
+++ read_snap_test.py
@@ -1,18 +1,21 @@
+import sys
+import numpy as np
+
+#import jpy
+sys.path.append('/home/timo/.snap/snap-python')
+#java_home_dir = '/usr/lib/jvm/java-21-openjdk-amd64/lib/server'
+#import jpy
 import esa_snappy
-from snappy import Product
 
 # Read the .dim file
-product = ProductIO.readProduct('path/to/your/file.dim')
-
-# Get the band you want to work with
-band = product.getBand('band_name')  # e.g., 'Amplitude_VV', 'Intensity_VH'
+product = esa_snappy.ProductIO.readProduct('/home/timo/Data/LVS1_snap/topo/S1A_IW_SLC__1SDV_20230328T134359_20230328T134427_047845_05BFAE_DD75_Orb_Stack_Ifg_Deb.dim')
 
 # Get the width and height
 width = product.getSceneRasterWidth()
 height = product.getSceneRasterHeight()
 
 # Convert band to numpy array
-import numpy as np
+
 band_data = np.zeros(width * height, dtype=np.float32)
 band.readPixels(0, 0, width, height, band_data)
 band_data = band_data.reshape(height, width)

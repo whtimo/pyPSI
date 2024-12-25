296f8b8950a61d11af1072fb8c253ead0017599b
Timo Balz <balz@whu.edu.cn>
2024-12-25 09:13:17 +0800

    Snappy Test Code

--- read_snap_test.py
+++ read_snap_test.py
@@ -8,15 +8,18 @@ sys.path.append('/home/timo/.snap/snap-python')
 import esa_snappy
 
 # Read the .dim file
-product = esa_snappy.ProductIO.readProduct('/home/timo/Data/LVS1_snap/topo/S1A_IW_SLC__1SDV_20230328T134359_20230328T134427_047845_05BFAE_DD75_Orb_Stack_Ifg_Deb.dim')
+product = esa_snappy.ProductIO.readProduct('/home/timo/Data/LVS1_snap/deburst/S1A_IW_SLC__1SDV_20230702T134404_20230702T134432_049245_05EBEA_A4DF_Orb_Stack_esd_deb.dim')
 
 # Get the width and height
 width = product.getSceneRasterWidth()
 height = product.getSceneRasterHeight()
+names = product.getBandNames()
+for name in names:
+    print(name)
 
 # Convert band to numpy array
 
-band_data = np.zeros(width * height, dtype=np.float32)
-band.readPixels(0, 0, width, height, band_data)
-band_data = band_data.reshape(height, width)
+#band_data = np.zeros(width * height, dtype=np.float32)
+#band.readPixels(0, 0, width, height, band_data)
+#band_data = band_data.reshape(height, width)
 

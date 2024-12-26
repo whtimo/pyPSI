16e3f37e538de2f5ff1ccad559da4cf2a39097f2
Timo Balz <balz@whu.edu.cn>
2024-12-26 08:47:06 +0800

    Continue snappy extract phases

--- extract_psc_phases_snappy.py
+++ extract_psc_phases_snappy.py
@@ -24,12 +24,12 @@ def read_complex_phase_snappy(product, band_name: str, width : int, height: int,
     # Read complex values at specified coordinates
     # rasterio expects (row, col) format, so we swap sample/line
     coords = [(line, sample) for sample, line in pixel_coords]
-    complex_values = []
+    phase_values = []
     for coord in coords:
-        complex_values.append(data[coord[0], coord[1]])
+        phase_values.append(data[coord[0], coord[1]])
 
     # Convert complex values to phase
-    phases = np.angle(complex_values)
+    phases = np.array(phase_values)
 
     return phases
 
@@ -71,7 +71,7 @@ def extract_ps_phases(ps_csv_path: str,
         date = extract_date_from_band_name(phase_band)
 
         # Read phase values
-        phases = read_complex_phase_snappy(phase_band, pixel_coords)
+        phases = read_complex_phase_snappy(product, phase_band, width, height, pixel_coords)
 
         # Add to results dictionary
         results[date] = phases
@@ -87,10 +87,10 @@ if __name__ == "__main__":
     # PS_CSV_PATH = "path/to/ps_coordinates.csv"
     # INTERFEROGRAM_DIR = "path/to/interferogram/directory"
     # OUTPUT_CSV_PATH = "path/to/output/ps_phases.csv"
-    PS_CSV_PATH = "/home/timo/Data/LasVegasDesc/psc.csv"
+    PS_CSV_PATH = "/home/timo/Data/LVS1_snap/psc.csv"
     input_dim_file = '/home/timo/Data/LVS1_snap/topo_subset/subset_0_of_S1A_IW_SLC__1SDV_20230702T134404_20230702T134432_049245_05EBEA_A4DF_Orb_Stack_esd_deb_ifg.dim'
 
-    OUTPUT_CSV_PATH = "/home/timo/Data/LasVegasDesc/psc_phases.csv"
+    OUTPUT_CSV_PATH = "/home/timo/Data/LVS1_snap/psc_phases.csv"
 
     # Extract phases and save to CSV
     extract_ps_phases(PS_CSV_PATH, input_dim_file, OUTPUT_CSV_PATH)
